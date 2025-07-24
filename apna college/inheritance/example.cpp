ab8545e26c9291df0264e52c7b3508a8ec1ad245
DishuSingla01 <singladishu815@gmail.com>
2025-07-24 14:20:25 +0530

    Learn Inheritance

---
+++ apna college/inheritance/example.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Person{
+  public:
+  int age;
+  string name; 
+
+  Person(){
+
+  }
+};
+
+class Student: public Person{
+public:
+  int rollno;
+
+  Student(int age,string name,int rollno){
+    this->age=age;
+    this->name=name;
+    this->rollno=rollno;
+  }
+
+  void getinfo(){
+    cout<<"Name:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+    cout<<"Roll Number:"<<rollno<<endl;
+  }
+};
+
+int main(){
+  Student s1(19,"Dishu Singla",1043);
+  s1.getinfo();
+}
